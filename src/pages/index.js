6bc976e3d4f9921a7ecc6c95aba6d817ca9e68a2
Jordan D. <contact@jordand.fr>
2024-07-01 16:07:25 +0200

    rename intro to welcome

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
+            to="/docs/bienvenue">
             Créer un tutoriel sur la documentation
           </Link>
         </div>
